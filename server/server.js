634211ed404bc3dc674fa153fd4e3babe466cc41
glshahar <galsh20@gmail.com>
2021-10-15 16:41:18 +0300

    add start2

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
+    console.log(`publicPath: ${publicPath}`);
    res.sendFile(path.join(publicPath, 'index.html'));
 });
 app.listen(port, () => {
